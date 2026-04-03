fdb34c079f553e57462b1eeb4c0ea7c90f8682ea
ashenafi <ashenafiadola05@gmail.com>
2026-04-03 22:36:45 +0300

    setted up the database model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    profile = models.ImageField(upload_to='profiles/', blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+    
+class Message(models.Model):
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    reciever = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    body = models.CharField(max_length=500)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    edited_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[:20]
