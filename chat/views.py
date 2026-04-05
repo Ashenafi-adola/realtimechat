10842162b4255f56378685d9aa96396dac616c93
ashenafi <ashenafiadola05@gmail.com>
2026-04-05 21:54:29 +0300

    created the sign in page view

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import generic
 from . models import Message, CustomUser
 from . forms import CustomUserCreationForm, MessageForm
+from django.contrib.auth import login, logout, authenticate
 
 def signUpPage(request):
     form = CustomUserCreationForm()
@@ -14,5 +15,17 @@ def signUpPage(request):
             return redirect('home')
     context = {
         'form':form,
+    }
+    return render(request, 'chat/authpage.html', context)
+
+def signInPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        user = authenticate(request, username, password)
+        if user != None:
+            return redirect('home')
+    context = {
+
     }
     return render(request, 'chat/authpage.html', context)
