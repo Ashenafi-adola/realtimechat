4c32a260cbcf0a4db3b0b20776a84f6f4295a2db
ashenafi <ashenafiadola05@gmail.com>
2026-04-05 21:50:08 +0300

    created the signup view

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from . models import Message, CustomUser
+from . forms import CustomUserCreationForm, MessageForm
+
+def signUpPage(request):
+    form = CustomUserCreationForm()
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            return redirect('home')
+    context = {
+        'form':form,
+    }
+    return render(request, 'chat/authpage.html', context)
